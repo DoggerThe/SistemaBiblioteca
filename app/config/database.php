bf8009c7be39e34a6706d54a75de856463c3dfe2
Roger <rabs0707@gmail.com>
2025-04-26 16:02:18 -0500

    Creacion de la conexion de DB

---
+++ app/config/database.php
@@ -0,0 +1,20 @@
+<?php
+class Database{
+    private $host = 'localhost';
+    private $db_name = 'biblioteca_mvc';
+    private $username = 'root';
+    private $password = '';
+    public $conn;
+
+    public function connect(){
+        $this->conn = null;
+        try{
+            $this -> conn =  new PDO ("mysql:host = {$this-host};dbname={$this->db_name};charset=utf8mb4", $this -> username, $this -> password);
+            $this ->conn ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "conexion exitante";
+        }catch (PDOException $exception){
+            echo "error de conexion: ". $exception -> getMessage();
+        }
+        return $this->conn;
+    }
+}
