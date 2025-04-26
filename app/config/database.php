14495540d98870797e447e385b75eeb8c34a6443
Roger <rabs0707@gmail.com>
2025-04-26 18:32:06 -0500

    Update database config

--- app/config/database.php
+++ app/config/database.php
@@ -3,13 +3,13 @@ class Database{
     private $host = 'localhost';
     private $db_name = 'biblioteca_mvc';
     private $username = 'root';
-    private $password = '';
+    private $password = 'root';
     public $conn;
 
     public function connect(){
         $this->conn = null;
         try{
-            $this -> conn =  new PDO ("mysql:host = {$this-host};dbname={$this->db_name};charset=utf8mb4", $this -> username, $this -> password);
+            $this -> conn =  new PDO ("mysql:host = {$this->host};dbname={$this->db_name};charset=utf8mb4", $this -> username, $this -> password);
             $this ->conn ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             echo "conexion exitante";
         }catch (PDOException $exception){
