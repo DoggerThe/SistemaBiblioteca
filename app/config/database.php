c59a209a5d4f777f6b939fe0b77f5844c1efe26d
Roger <rabs0707@gmail.com>
2025-04-26 18:34:37 -0500

    Update database config

--- app/config/database.php
+++ app/config/database.php
@@ -11,7 +11,7 @@ class Database{
         try{
             $this -> conn =  new PDO ("mysql:host = {$this->host};dbname={$this->db_name};charset=utf8mb4", $this -> username, $this -> password);
             $this ->conn ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo "conexion exitante";
+            echo "conexion exitante.";
         }catch (PDOException $exception){
             echo "error de conexion: ". $exception -> getMessage();
         }
