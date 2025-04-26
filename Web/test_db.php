838e8ad1673f738ede2246553fbf605dc644e735
Roger <rabs0707@gmail.com>
2025-04-26 14:24:59 -0500

    Archivos removidos y Estructurados

--- Web/test_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $pdo = new PDO('mysql:host=localhost;dbname=mysql', 'root', 'root');
-    echo "Conexión exitosa a MySQL Community Server";
-} catch (PDOException $e) {
-    echo "Error de conexión: " . $e->getMessage();
-}
-?>
